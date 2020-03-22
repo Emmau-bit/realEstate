6b35e2b20e533581c2c6dbeb543f566ea141e430
Your Name <you@example.com>
2020-03-22 08:34:08 +0200

    First commit

--- server.js
+++ server.js
@@ -52,12 +52,16 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const compression = require('compression');
+const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const expressValidator = require('express-validator');
 require('dotenv').config();
+
+app.use(compression());
 
 const app = express();
 
@@ -83,6 +87,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/status', require('./routes/api/status'));
 app.use('/api/property', require('./routes/api/property'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 

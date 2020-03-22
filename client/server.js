138f3066bc48a0d68e099ce4d8ab37bf4788413f
Your Name <you@example.com>
2020-03-22 08:06:48 +0200

    First commit

--- client/server.js
+++ client/server.js
@@ -4,11 +4,20 @@ const path = require('path');
 const app = express();
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  //   // Set static folder
+  app.use(express.static('build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 
